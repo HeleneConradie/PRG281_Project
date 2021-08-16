8574944398457dbc3660da9d49c053d93492165a
HeleneConradie <heleneconradie01@gmail.com>
2021-08-16 19:05:00 +0200

    Created Scoreboard form

---
+++ SkyBeat/SkyBeat/Scoreboard.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace SkyBeat
+{
+    partial class frmScoreboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScoreboardHeading1 = new System.Windows.Forms.Label();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.lblScoreboardUsername = new System.Windows.Forms.Label();
+            this.pnl2 = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnl3 = new System.Windows.Forms.Panel();
+            this.lblCongratulations = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScoreboardHeading1
+            // 
+            this.lblScoreboardHeading1.AutoSize = true;
+            this.lblScoreboardHeading1.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreboardHeading1.Location = new System.Drawing.Point(250, 9);
+            this.lblScoreboardHeading1.Name = "lblScoreboardHeading1";
+            this.lblScoreboardHeading1.Size = new System.Drawing.Size(157, 33);
+            this.lblScoreboardHeading1.TabIndex = 0;
+            this.lblScoreboardHeading1.Text = "Scoreboard";
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.Black;
+            this.pnl1.Location = new System.Drawing.Point(144, 82);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(2, 220);
+            this.pnl1.TabIndex = 1;
+            // 
+            // lblScoreboardUsername
+            // 
+            this.lblScoreboardUsername.AutoSize = true;
+            this.lblScoreboardUsername.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreboardUsername.Location = new System.Drawing.Point(49, 82);
+            this.lblScoreboardUsername.Name = "lblScoreboardUsername";
+            this.lblScoreboardUsername.Size = new System.Drawing.Size(77, 18);
+            this.lblScoreboardUsername.TabIndex = 2;
+            this.lblScoreboardUsername.Text = "Username";
+            // 
+            // pnl2
+            // 
+            this.pnl2.BackColor = System.Drawing.Color.Black;
+            this.pnl2.Location = new System.Drawing.Point(250, 82);
+            this.pnl2.Name = "pnl2";
+            this.pnl2.Size = new System.Drawing.Size(2, 220);
+            this.pnl2.TabIndex = 3;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(174, 82);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(46, 18);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "Score";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(279, 82);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(40, 18);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "Time";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SkyBeat.Properties.Resources.Music_Notes;
+            this.pictureBox1.Location = new System.Drawing.Point(343, 183);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnl3
+            // 
+            this.pnl3.BackColor = System.Drawing.Color.Black;
+            this.pnl3.Location = new System.Drawing.Point(28, 103);
+            this.pnl3.Name = "pnl3";
+            this.pnl3.Size = new System.Drawing.Size(320, 2);
+            this.pnl3.TabIndex = 7;
+            // 
+            // lblCongratulations
+            // 
+            this.lblCongratulations.AutoSize = true;
+            this.lblCongratulations.Font = new System.Drawing.Font("Bahnschrift Condensed", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCongratulations.Location = new System.Drawing.Point(433, 57);
+            this.lblCongratulations.Name = "lblCongratulations";
+            this.lblCongratulations.Size = new System.Drawing.Size(239, 48);
+            this.lblCongratulations.TabIndex = 8;
+            this.lblCongratulations.Text = "Congratulations!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Helene";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "9/10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(279, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "25 s";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Henco";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(174, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "7/10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(279, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "34 s";
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(549, 118);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(104, 19);
+            this.lblWinner.TabIndex = 15;
+            this.lblWinner.Text = "Helene Wins!";
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.BackColor = System.Drawing.Color.White;
+            this.btnPlayAgain.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayAgain.Location = new System.Drawing.Point(12, 353);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(150, 30);
+            this.btnPlayAgain.TabIndex = 16;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(187, 353);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 30);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmScoreboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(684, 411);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCongratulations);
+            this.Controls.Add(this.pnl3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnl2);
+            this.Controls.Add(this.lblScoreboardUsername);
+            this.Controls.Add(this.pnl1);
+            this.Controls.Add(this.lblScoreboardHeading1);
+            this.MaximizeBox = false;
+            this.Name = "frmScoreboard";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scoreboard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScoreboardHeading1;
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Label lblScoreboardUsername;
+        private System.Windows.Forms.Panel pnl2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnl3;
+        private System.Windows.Forms.Label lblCongratulations;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
